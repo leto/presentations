d62ebf83f1ce7b565aa9d36c4355c12520e5ed6f
Jonathan "Duke" Leto <jonathan@leto.net>
2013-10-22 13:24:46 -0700

    add Git::Raw example

---
+++ 2013/sf.pm/git-raw-example.pl
@@ -0,0 +1,7 @@
+my $url  = 'git://github.com/git/git.git';
+my $repo = Git::Raw::Repository->clone($url,
+            'git', { bare => 0 });
+# print all the tags of the repository
+for my $tag (@{ $repo -> tags }) {
+    say $tag -> name;
+}
